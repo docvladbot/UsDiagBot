b97d30aacefe950ed8483cdb2f14c09abb90949f
docvladbot <docvlad11@gmail.com>
2025-05-10 13:19:02 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,9 +1,6 @@
 
 from models.fetalnet import FetalNet
-from models.fetalclip import FetalCLIP
 
-# Пример инициализации моделей
-fetalnet_model = FetalNet()
-fetalclip_model = FetalCLIP()
-
-print("Модели успешно загружены.")
+if __name__ == "__main__":
+    model = FetalNet()
+    print("FetalNet model loaded.")
