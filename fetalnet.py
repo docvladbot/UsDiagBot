4c39a5ccf44438200907a17dab0a6bc4e58d8666
docvladbot <docvlad11@gmail.com>
2025-05-10 19:26:50 +0300

    Add files via upload

--- fetalnet.py
+++ fetalnet.py
@@ -1,10 +1,3 @@
-
-import torch.nn as nn
-
-class FetalNet(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.layer = nn.Linear(224 * 224, 2)
-
-    def forward(self, x):
-        return self.layer(x.view(x.size(0), -1))
+class FetalNet:
+    def predict(self, image):
+        return "FetalNet prediction"  # Заглушка
