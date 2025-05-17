dd9d9b2fb7f7e929993fee6e31de6175d5fe92ef
docvladbot <docvlad11@gmail.com>
2025-05-17 22:44:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,21 @@
+from telegram.ext import Updater, CommandHandler
 
+# Вставляем твой токен
+TOKEN = "7555855256:AAHfTwjfkEaPz3Z89RBD41Q5Y-i5lEGyHms"
+
+# Обработчик команды /start
+def start(update, context):
+    update.message.reply_text("Бот запущен и работает!")
+
+# Основной запуск
+def main():
+    updater = Updater(token=TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
